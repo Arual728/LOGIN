e0ab97a8c5c0f3682ba019c96a846fcdf83e8ebe
Arual728 <Arual728@users.noreply.github.com>
2016-06-14 00:12:43 +0000

    Add files via upload

---
+++ seleccion.php
@@ -0,0 +1,27 @@
+<?php  include("apertura.php"); 
+$ssql = "select id, usuario from usuarios"; 
+$db_blog = mysql_query($ssql); 
+while($fila = mysql_fetch_array($db_blog)) { 
+echo'<a href="editar.php?id=' . $fila['id'] . '">Editar</a> '; 
+echo $fila["usuario"] . "<br />";}  
+?>
+<style type="text/css"> 
+<center>
+*{ 
+    font-size: 14px; 
+} 
+body{ 
+background:#aaa; 
+} 
+while,echo { 
+    background: none repeat scroll 0 0 #F1F1F1; 
+    border: 1px solid #DDDDDD; 
+    font-family: sans-serif; 
+    margin: 0 auto; 
+    padding: 50px; 
+    width: 310px; 
+    box-shadow:0px 0px 30px black; 
+    border-radius:10px; 
+}
+</center>
+</style>
